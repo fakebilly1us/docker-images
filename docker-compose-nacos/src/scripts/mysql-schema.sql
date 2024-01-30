29b3f2748c7122adb4bed3f613eae59173f9185e
FakeBilly <fakebilly.work@outlook.com>
2024-01-30 11:18:28 +0800

    update docker compose 2.0

--- docker-compose-nacos/src/scripts/mysql-schema.sql
+++ docker-compose-nacos/src/scripts/mysql-schema.sql
@@ -224,6 +224,6 @@ CREATE TABLE `permissions` (
     UNIQUE INDEX `uk_role_permission` (`role`,`resource`,`action`) USING BTREE
 );
 
-INSERT INTO users (username, password, enabled) VALUES ('nacos', '$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuWj7VEOJhhZkDrxfvUu', TRUE);
+INSERT INTO users (username, password, enabled) VALUES ('nacos', '$2a$10$b5Cr7nNPk8zw1opimHy/TOigdJnZp9HHUBylPc4IUvGBmO1/.0aLC', TRUE);
 
 INSERT INTO roles (username, role) VALUES ('nacos', 'ROLE_ADMIN');
